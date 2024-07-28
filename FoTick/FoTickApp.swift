34788803f5f9638819b5cb80fac377c1ca622cac
Nguyen Manh Hung <manhhunghd2000@gmail.com>
2024-07-28 12:34:03 +0700

    Initial Commit

---
+++ FoTick/FoTickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoTickApp.swift
+//  FoTick
+//
+//  Created by Nguyễn Mạnh Hùng on 28/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FoTickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
